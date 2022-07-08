1555d92077f7345f1ff2568309b9dbf444d9aef8
Alva Rani James <alvarani@gmail.com>
2022-07-08 21:58:49 +0200

    Update run_basic_export.sh

--- AMD_PD/unidb_scripts/shell/run_basic_export.sh
+++ AMD_PD/unidb_scripts/shell/run_basic_export.sh
@@ -1,2 +1,3 @@
 #make sure you are runing the basic_export in python2
-cat gene_list|xargs -I% echo "python basic_export.py "
+cat gene_list|xargs -I% echo "python basic_export.py % & " >extract_genes_patients.sh
+sh extract_genes_patients.sh
